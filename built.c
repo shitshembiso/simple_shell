9fb5602eb0a63f6ddaa0c6de7027b1e4decd3615
shitshembiso <ntlharhi74@gmail.com>
2023-11-16 08:00:34 -0800

    simple shell. stop the cap

--- built.c
+++ built.c
@@ -1,4 +1,3 @@
-84% of storage used … If you run out, you won't have enough storage to create, edit and upload files. Get 100 GB of storage for R 34,99 R 8,99/month for 3 months.
 #include "shell.h"
 
 /**
